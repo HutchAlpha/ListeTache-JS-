00499af545aba6811d8260b6d03166f2135d17c2
Quentin BOISSET <82939856+HutchAlpha@users.noreply.github.com>
2023-12-23 09:31:15 +0100

    Ajout couleur lorsque la tâche et validé+ checkbox
    Je repenche sur le projet car gros projet pro a terminer

--- JS/node.js
+++ JS/node.js
@@ -2,10 +2,8 @@ document.addEventListener('DOMContentLoaded', function () {
     const formElement = document.getElementById('taskForm');
     const tacheInputElement = document.getElementById('Tache');
     const tasksTableBody = document.getElementById('tasksTableBody');
-    var supprimer = document.getElementsByClassName('supprimer');
 
     if (formElement && tacheInputElement && tasksTableBody) {
-        // Gestionnaire d'événements pour le formulaire
         function onSubmitForm(event) {
             event.preventDefault();
 
@@ -15,32 +13,44 @@ document.addEventListener('DOMContentLoaded', function () {
             const newRow = document.createElement('tr');
             newRow.innerHTML = `
                 <td>${tacheValue}</td>
-                <td>Non</td>
+                <td class="center"><input type="checkbox" id="check" name="check"/></td>
                 <td><button class="supprimer">Supprimer</button></td>
             `;
 
             // Ajout de la nouvelle ligne à la fin du corps du tableau
             tasksTableBody.appendChild(newRow);
 
+            const checkbox = newRow.querySelector('input[type="checkbox"]');
+            checkbox.addEventListener('change', function() {
+                changeColor(this);
+            });
+
             // Effacez le champ de saisie après soumission
             tacheInputElement.value = '';
         }
 
         formElement.addEventListener('submit', onSubmitForm);
+
+        tasksTableBody.addEventListener('click', function(event) {
+            if (event.target.classList.contains('supprimer')) {
+                var row = event.target.closest('tr');
+
+                if (row) {
+                    row.remove();
+                }
+            }
+        });
+
+        function changeColor(checkbox) {
+            const row = checkbox.closest('tr');
+            if (checkbox.checked) {
+                row.style.backgroundColor = 'rgb(219, 219, 162)';
+            } else {
+                row.style.backgroundColor = ''; 
+            }
+        }
+        
     } else {
         console.error('Éléments introuvables dans le DOM');
     }
-
-    tasksTableBody.addEventListener('click', function(event) {
-        // Vérifiez si le clic a été effectué sur un bouton avec la classe "supprimer"
-        if (event.target.classList.contains('supprimer')) {
-            // Obtenez la ligne parente du bouton cliqué (tr)
-            var row = event.target.closest('tr');
-
-        if (row) {
-            row.remove();
-        }
-    }
-    });
-
 });
